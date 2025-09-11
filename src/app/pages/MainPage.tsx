4da914c5c43610b232399e0b78993c371d26ada7
go2020h <go2020.hidane.inc@gmail.com>
2025-09-12 07:07:04 +0900

    fix

--- src/app/pages/MainPage.tsx
+++ src/app/pages/MainPage.tsx
@@ -772,7 +772,7 @@ const MainPage = () => {
                 <div className="relative pb-[56.25%] h-0 overflow-hidden">
                   <iframe 
                     className="absolute top-0 left-0 w-full h-full"
-                    src="https://www.youtube.com/embed/xg6tmpVTgRw" 
+                    src="https://www.youtube.com/watch?v=v85WMKS2coY" 
                     title="【LIVE】渋谷愛ビジョン・宮益坂交差点ライブカメラ／『SHIBUYA AI Vision』" 
                     frameBorder="0" 
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
