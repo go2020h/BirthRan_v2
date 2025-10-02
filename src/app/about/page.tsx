83e90cd2ff6c91a13d6e7a5ce8962b5e0725e87c
go2020h <go2020.hidane.inc@gmail.com>
2025-10-02 18:33:08 +0900

    fix

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -330,7 +330,7 @@ const ProgramPage: React.FC = () => {
                   個性豊かなバスランのパーソナリティが得意とする分野、世の中に発信したい愛ある情報を紹介するコーナーです。<br/>
                   このコーナーを聴いたリスナーは、パーソナリティに素敵な情報を『ありがとう』と伝えたくなること間違いなし！こうご期待！！
                 </p>
-              </div>
+              </div>*/}
 
               <div className="bg-white p-6 rounded-lg shadow-md border-l-4 border-[#FF6B6B] hover:shadow-lg transition-all duration-300">
                 <h3 className="text-2xl font-bold text-[#1a3a6c] mb-4 flex items-center">
