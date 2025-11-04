e50fc4e348651e5f7bc24c595d8d23b8f9941dfe
go2020h <go2020.hidane.inc@gmail.com>
2025-11-04 22:05:03 +0900

    fix

--- src/app/pages/MainPage.tsx
+++ src/app/pages/MainPage.tsx
@@ -997,10 +997,10 @@ const MainPage = () => {
                     </div>
                     <div className="md:w-2/3">
                       <p className="text-gray-700 mb-4">
-
+                        <br></br>
                       </p>
                       <p className="text-gray-700 mt-3">
-                        
+                        <br></br>
                       </p>
                       <div className="mt-4 text-center">
                         <a 
